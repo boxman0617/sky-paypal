8f84fc96feeba7b2a55e614a4fa571f552e64024
Alan Tirado <atirado0617@gmail.com>
2014-06-30 00:05:51 -0700

    bugfix on up.php
    Fixing where config file comes from

--- up.php
+++ up.php
@@ -1,6 +1,6 @@
 <?php
 SkyL::Import(Plugin::GetFile($plugin));
-$__PluginDir__ = Plugin::GetPluginDir($plugin);
+$__PluginDir__ = Plugin::GetLocalPluginDir($plugin);
 $config = $__PluginDir__.'/config.php';
 if(file_exists($config))
 {
